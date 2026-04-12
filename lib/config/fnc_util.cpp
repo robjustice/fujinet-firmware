7dbf3db897aa8ef39aa6cddd93ed3ad37c3ff15d
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2026-04-11 19:45:56 -0500

    [fnc] fix RS232 baud rate (#1243)
    Fix so that config file properly persists baud rate in [Serial] for
    RS232 devices.

--- lib/config/fnc_util.cpp
+++ lib/config/fnc_util.cpp
@@ -135,6 +135,12 @@ fnConfig::section_match fnConfig::_find_section_in_line(std::string &line, int &
                 return SECTION_BOS;
             }
 #endif
+#ifdef BUILD_RS232
+            else if (strncasecmp("Serial", s1.c_str(), 6) == 0)
+            {
+                return SECTION_SERIAL;
+            }
+#endif /* BUILD_RS232 */
         }
     }
     return SECTION_UNKNOWN;
